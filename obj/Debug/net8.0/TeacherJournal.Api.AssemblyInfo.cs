a36d67768f7d792041aeeecec47191d104fdb8fb
yuxra <rasikahrasikah3@gmail.com>
2026-08-03 14:30:35 +0700

    Initial backend

--- obj/Debug/net8.0/TeacherJournal.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TeacherJournal.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeacherJournal.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8b4e2bfa99dca9f92c777bb32d95765bc07b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeacherJournal.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeacherJournal.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
